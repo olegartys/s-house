b299a3d16084e12db33c79b8dbc4fa5c98567071
olegartys <olegartys@gmail.com>
2016-04-02 14:28:17 +0300

    Удалил директории .idea изо всех подпапок

---
+++ SHouseServerComponent/QueryAnalyzingPacket/CMakeFiles/QueryAnalyzingPacket.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/QueryAnalyzingPacket.dir/main.o"
+  "CMakeFiles/QueryAnalyzingPacket.dir/src/QueryListener.o"
+  "CMakeFiles/QueryAnalyzingPacket.dir/src/QueryHandler.o"
+  "CMakeFiles/QueryAnalyzingPacket.dir/src/QueryParser.o"
+  "CMakeFiles/QueryAnalyzingPacket.dir/src/Utils.o"
+  "QueryAnalyzingPacket.pdb"
+  "QueryAnalyzingPacket"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/QueryAnalyzingPacket.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
