a912d90c40da35c11931c1a5e51ba5c015ed867f
olegartys <olegartys@gmail.com>
2016-04-08 13:49:11 +0300

    Удалил SensorInfo и SensorName

--- SHouseServerComponent/QueryAnalyzingPacket/include/Utils.h
+++ SHouseServerComponent/QueryAnalyzingPacket/include/Utils.h
@@ -7,13 +7,8 @@
 
 #include <string>
 #include <sstream>
-#include "SensorInfo.h"
 
-std::string to_string(const SensorName& sensorName);
-std::ostream& operator<< (std::ostream& str, const SensorName& sensorName);
 
-std::string to_string(const SensorInfo& sensorInfo);
-std::ostream& operator<< (std::ostream& str, const SensorInfo& i);
 
 
 #endif //QUERYANALYZINGPACKET_UTILS_H
--- SHouseServerComponent/QueryAnalyzingPacket/src/Utils.cpp
+++ SHouseServerComponent/QueryAnalyzingPacket/src/Utils.cpp
@@ -3,27 +3,3 @@
 //
 
 #include "Utils.h"
-
-std::string to_string(const SensorName& sensorName) {
-    std::stringstream ss;
-    ss << sensorName.sensorUserName << "," << sensorName.sensorSystemName;
-    return ss.str();
-}
-
-
-std::string to_string(const SensorInfo& sensorInfo) {
-    std::stringstream ss;
-    ss << to_string(sensorInfo.sensorName) << "," << sensorInfo.queryType << "," << sensorInfo.data;
-    return ss.str();
-}
-
-std::ostream& operator<< (std::ostream& str, const SensorName& sensorName) {
-    str << to_string(sensorName);
-    return str;
-}
-
-
-std::ostream& operator<< (std::ostream& str, const SensorInfo& sensorInfo) {
-    str << to_string(sensorInfo);
-    return str;
-}
